0eafc62587a4a63545afeb80ea84359d693639b3
ANGELMG79 <129173334+ANGELMG79@users.noreply.github.com>
2023-04-14 14:17:18 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+const turno = document.querySelector("#turno");
+
+turno.addEventListener( "submit", validarFormulario )
+
+
+function validarFormulario(e){
+    e.preventDefault();
+    const nombre = document.querySelector("#nombre").value
+    const apellido = document.querySelector("#apellido").value
+    const fecha = document.querySelector("#fecha").value
+    const correo = document.querySelector("#correo").value
+
+    const respuesta = document.getElementById("respuesta");
+    respuesta.textContent = `El turno requerido por ${nombre} ${apellido} es el dia ${fecha}, se solicitara confirmacion por el correo ${correo}`
+}
